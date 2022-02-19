a9fba7f277a484126c4a51aa777a960ee7bb776a
"Monster-hash" <"monster0hash@gmail.com">
2022-02-19 22:08:22 +0800

    3

--- 9_dynamicByte.sol
+++ 9_dynamicByte.sol
@@ -8,8 +8,23 @@ contract DynamicByte{
         name[0] = 0x7a;
         name[1] = 0x68;//初始化完成
     }
+
     function getLength() view returns(uint)//查看当前动态数组长度
     {
-            return name.length;
+        return name.length;
+    }
+
+    function changeName()
+    {
+        name[0] = 0x88;
+
+    }
+    function changeLength()
+    {
+        name.length = 5;//扩充数组后也会全部初始化为0，0:bytes: 0x0000000000
+    }
+    function pushTest()        //push内置方法
+    {
+        name.push(0x99);//在原数组后添加新元素
     }
 }
